711e702438147a528017d203f8f29c65e1860346
Marknjo <marknjo7@gmail.com>
2022-01-12 13:08:56 +0300

    Natours#3: Refactor tours from app file to controller and routes files

---
+++ routes/toursRouter.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as toursController from '../controllers/toursController.js';
+
+// init router
+const router = express.Router();
+
+router
+  .route('/')
+  .get(toursController.getAllTours)
+  .post(toursController.createTour);
+
+router
+  .route('/:id')
+  .get(toursController.getTour)
+  .patch(toursController.updateTour)
+  .delete(toursController.deleteTour);
+
+// export router
+export default router;
