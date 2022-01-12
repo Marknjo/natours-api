e4a5b4dec1c61393ed598e586e5ff5c8d715dc1d
Marknjo <marknjo7@gmail.com>
2022-01-12 17:32:10 +0300

    NatoursL08#01: Setup local MongoDB server with Mongoose

--- server.js
+++ server.js
@@ -2,12 +2,23 @@
 import { env } from 'process';
 
 // 3rd PARTY IMPORTS
+import mongoose from 'mongoose';
 
 // LOCAL IMPORTS
 import './configs/configEnv.js';
 import app from './app.js';
 
 // DATABASE SETUP
+try {
+  mongoose.connect(env.DB_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log('DB connected successfully 😃😃😃...');
+} catch (error) {
+  console.error(`DB CONNECTION ERROR: 💥💥💥 \n${error.stack}`);
+}
 
 // LISTEN TO SERVER
 const host = env.HOST || 'localhost';
