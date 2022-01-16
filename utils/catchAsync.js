353c826771a011d1c3cc780ac591ff1fb7d8f924
Marknjo <marknjo7@gmail.com>
2022-01-16 14:27:55 +0300

    l08l09#recap#01: CatchAsync comments

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,9 @@
+/**
+ * Express Asyncronous Wrapper
+ * Centralizes Asyncrounous Errors by Delegating Error handling to the global error handler
+ * @param {Function} fn Asyncrounous Express handler
+ * @returns {Function} Express handler
+ */
 const catchAsync = fn => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
