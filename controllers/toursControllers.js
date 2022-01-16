848236c057f8dbe38ef0826cf366bc617c1002a7
Marknjo <marknjo7@gmail.com>
2022-01-16 21:12:54 +0300

    l08l09#recap#01: Implement API Features for pagination, filtering, sorting, and query field limits

--- controllers/toursControllers.js
+++ controllers/toursControllers.js
@@ -1,6 +1,8 @@
 // IMPORT
 
 // Local
+import APIFeature from '../helpers/apiFeatures.js';
+import Tour from '../models/toursModel.js';
 import catchAsync from '../utils/catchAsync.js';
 
 // MIDDLEWARE SETUP
@@ -9,12 +11,65 @@ import catchAsync from '../utils/catchAsync.js';
 // Get All Tours
 export const getAllTours = catchAsync(async (req, res, next) => {
   // API Features for Filtering Data. Sort, Fields, & Pagination
+  //   // 1). Filtering
+  //   const queryObj = { ...req.query };
+  //   // TODO: Implement allowable fields types
+  //   // Filtering Basic fields,sort,page,limit
+  //   const excludeQueryFields = ['fields', 'sort', 'page', 'limit'];
+  //   excludeQueryFields.map(field => delete queryObj[field]);
+
+  //   // Filtering Adavanced
+  //   const queryStr = JSON.parse(
+  //     JSON.stringify(queryObj).replace(
+  //       /\b(gte|gt|lte|lt)\b/,
+  //       match => `$${match}`
+  //     )
+  //   );
+
+  //   let query = Tour.find(queryStr);
+  //   // Helpers
+  //   const replaceCommaWithSpace = str => {
+  //     return str.split(',').join(' ');
+  //   };
+
+  //   // 2). Fields
+  //   const showFields = req.query.fields;
+  //   if (showFields) {
+  //     const cleanFields = replaceCommaWithSpace(showFields);
+  //     query = query.select(cleanFields);
+  //   } else {
+  //     query = query.select('-__v');
+  //   }
+  //   // 3). Sorting
+  //   const sortFields = req.query.sort;
+  //   if (sortFields) {
+  //     const cleanSortFields = replaceCommaWithSpace(sortFields);
+  //     query = query.sort(cleanSortFields);
+  //   } else {
+  //     query = query.sort('-createdAt');
+  //   }
+
+  //   // 4). Pagination
+  //   const page = +req.query.page || 1;
+  //   const limit = +req.query.limit || 100;
+  //   const skip = (page - 1) * limit;
+
+  //   query = query.skip(skip).limit(limit);
+
+  const features = new APIFeature(Tour.find(), req.query)
+    .filter()
+    .limitFields()
+    .sort()
+    .paginate();
+
+  const tours = await features.query;
 
   // Return Response
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
-      tours: 'Tours to come.',
+      tours,
     },
   });
 });
