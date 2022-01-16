76ea432267c611a7c68ddacda9089890e60c1d6a
Marknjo <marknjo7@gmail.com>
2022-01-16 13:54:14 +0300

    l08l09#recap#01: Define App error handling class

---
+++ utils/appError.js
@@ -0,0 +1,23 @@
+// Extend Error class
+// Mark message, statusCode, status, isOPerational, and prevent errors from tracing to the class.
+
+class AppError extends Error {
+  constructor(message, statusCode) {
+    // Set message
+    super(message);
+
+    // Set status Code
+    this.statusCode = statusCode;
+
+    // Set Status Message
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+    // Set is Operational
+    this.isOperational = true;
+
+    // Prevent errors from tracing to the this class
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
