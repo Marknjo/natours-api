33e41d8cef31a75ba99778243eaca4b38597e608
Marknjo <marknjo7@gmail.com>
2022-01-17 20:26:17 +0300

    L10#01: Fix bug - fixed deleted code line that was supposed to fetch user of the JWT token id

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,6 +108,8 @@ export const protect = catchAsync(async (req, res, next) => {
 
   const decodedToken = await promisify(jwt.verify)(token, env.JWT_SECRET);
 
+  const currentUser = await User.findById(decodedToken.id);
+
   if (!currentUser) {
     return next(
       new AppError(
