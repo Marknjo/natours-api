b5974869515a6310345fbba3d6f3f905d7abedb9
Marknjo <marknjo7@gmail.com>
2022-01-25 18:40:32 +0300

    L11#01: Handle 2 decimal place display of ratings average

--- models/toursModel.js
+++ models/toursModel.js
@@ -64,6 +64,7 @@ const tourSchema = new Schema(
       default: 4.5,
       min: [1, 'A tour rating must be above 1'],
       max: [5, 'A tour rating must be below 5 '],
+      set: val => Math.round(val * 10) / 10,
     },
 
     // Tour Ratings Quantity
