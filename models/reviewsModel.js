6bf8959c322f0122f907002522e7dfaa3e6da474
Marknjo <marknjo7@gmail.com>
2022-01-25 12:41:01 +0300

    L10#01: Defined a reviews model

---
+++ models/reviewsModel.js
@@ -0,0 +1,55 @@
+// IMPORTS
+import mongoose from 'mongoose';
+
+// SETUP
+const { Schema, model } = mongoose;
+
+// Review Schema Definition
+// review description, tour Reference, user reference, createdAt and UpdatedAt, rating
+const reviewSchema = new Schema(
+  {
+    // Review Description
+    review: {
+      type: String,
+      required: [true, 'You must include your review details'],
+      trim: true,
+    },
+    user: {
+      type: Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must have a user'],
+    },
+    tour: {
+      type: Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A review must belong to a tour'],
+    },
+    rating: {
+      type: Number,
+      max: [5, 'A review rating must be below 5'],
+      min: [1, 'A review rating must be above 1'],
+      required: [
+        true,
+        'You must submit your review rating score between 1 - 5',
+      ],
+    },
+  },
+  {
+    // timestamp
+    timestamps: true,
+
+    // virtuals
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+// VIRTUAL FIELDS,
+// INDEXES,
+// MIDDLEWARES
+// QUERY METHODS
+// MODEL DECLARATION
+const Review = model('Review', reviewSchema);
+
+// EXPORT
+export default Review;
