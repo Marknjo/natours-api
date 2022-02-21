27202623dcfbb751c50fdb9f4e46b804a5fbd617
Marknjo <marknjo7@gmail.com>
2022-02-21 10:43:24 +0300

    NR#01-Tours: Implement an async await catch error for express handlers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,10 @@
 // IMPORT MODULES
 // 3rd Party Locals
 
+// Local imports
+import AppError from '../library/appErrors.js';
+import catchAsync from '../library/catchAsyc.js';
+
 // HELPER FUNCTIONS @TODO: Export them to separate utility file
 // MIDDLEWARES HANDLERS  @TODO: Export them to separate utility file
 // SINGLE FEATURE HANDLERS
@@ -10,14 +14,16 @@
 /**
  * Get A single Tour
  */
-export const getAllTour = (req, res) => {
+export const getAllTour = catchAsync(async (res, req, next) => {
+  return next(new AppError('Test Error', 500));
+
   res.status(200).json({
     status: 'success',
     data: {
       tours: 'To be implemented soon...',
     },
   });
-};
+});
 
 // AGGREGATE HANDLERS
 // @TODO: Implement getToursStatsByDifficulty, getMontlyPlans
