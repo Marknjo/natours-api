89ef9b91d03b5872867047388a276ec43a2e288a
Marknjo <marknjo7@gmail.com>
2022-02-23 07:50:45 +0300

    NR#01-Tours: Add API features for Fetching all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,7 @@
 // Local imports
 import AppError from '../library/appErrors.js';
 import catchAsync from '../library/catchAsyc.js';
+import Tour from '../models/tourModel.js';
 
 // HELPER FUNCTIONS @TODO: Export them to separate utility file
 // MIDDLEWARES HANDLERS  @TODO: Export them to separate utility file
@@ -14,13 +15,23 @@ import catchAsync from '../library/catchAsyc.js';
 /**
  * Get A single Tour
  */
-export const getAllTour = catchAsync(async (res, req, next) => {
-  return next(new AppError('Test Error', 500));
+export const getAllTour = catchAsync(async (req, res, next) => {
+  // @TODO: Implement advancedFindFeatures (filters, sort, fields, pagination)
+
+  // Get all tours
+  let tours = await Tour.find();
+  console.log('Tour');
+
+  // If there is no tours -> Send a message instead
+  if (tours.length < 1) {
+    tours = 'There is no tours returned from this request';
+  }
 
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
-      tours: 'To be implemented soon...',
+      tours,
     },
   });
 });
