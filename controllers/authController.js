9295ce69537e1040253dd82647793958b96f9e4d
Marknjo <marknjo7@gmail.com>
2022-03-01 10:25:57 +0300

    NR#01-Tours: Implement token hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,23 @@
 // IMPORT MODULES
+// Global
+import { env } from 'process';
+import { promisify } from 'util';
+
+// 3rd party
+import jwt from 'jsonwebtoken';
 
 // HELPERS
 // @TODO: signAndUpdate, signToken,
+/**
+ * Signs a JWT token
+ * @param {String} id user id
+ * @param {Boolean} remember sets user to remeber user for 1 week
+ */
+const signJWTtoken = async (id, remember) => {
+  return await promisify(jwt.sign)({ id }, env.JWT_SECRET, {
+    expiresIn: remember ? env.JWT_EXPIRES_IN_WEEK : env.JWT_EXPIRES_IN_DAY,
+  });
+};
 
 // MIDDLEWARES
 
