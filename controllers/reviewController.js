d756173a911b1c9b06940b4ed12cebc417428b80
Marknjo <marknjo7@gmail.com>
2022-03-09 08:24:16 +0300

    NR#01-Reviews: Add review base files (model|controller|router)

---
+++ controllers/reviewController.js
@@ -0,0 +1,17 @@
+/// IMPORT DEPENDENCIES
+// GLOBAL IMPORTS
+// 3RD PARTY IMPORTS
+// LOCAL IMPORTS
+
+/// LOCAL HELPERS
+
+/// MIDDLEWARES HANDLERS
+// @TODO: Implement -> filterGetReviews
+
+/// SINGLE FEATURE HANDLERS
+// @TODO: Implement -> getAllTourReviews,
+
+/// CRUD HANDLERS - FACTORY
+// @TODO: Implement -> getAllReviews, getReview, createReview, deleteReview, updateReview
+
+/// AGGREGATE HANDLERS (IF ANY)
---
+++ routers/reviewRouter.js
@@ -0,0 +1,14 @@
+/// IMPORT DEPENDENCIES
+// 3rd Party Imports
+// local Imports
+/// Init Router
+
+/// ROUTES DEFINATIONS
+
+// MIDDLEWARES ROUTES
+
+/// SINGLE ROUTES
+
+/// CRUD ROUTES
+
+/// EXPORT ROUTER
