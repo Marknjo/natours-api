222ebf018517be5f823f26caf4907611847883cf
Marknjo <marknjo7@gmail.com>
2022-03-12 07:29:57 +0300

    NR#01-Client: Add basic overview handler

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,6 +1,25 @@
 // MODULES IMPORT
 
+import catchAsync from '../library/catchAsyc.js';
+import Tour from '../models/tourModel.js';
+
 /// MIDDLEWARE
 //@TODO: getOverview getTour (/loginUser /logoutUser /signupUser -> No handlers, API requests)
 
-/// ROUTE HANDLERS
+/**
+ * Get all tours
+ */
+
+export const getOverview = catchAsync(async (req, res, next) => {
+  // Fetch all tours -> filter only active tours
+  const tours = await Tour.find();
+
+  // No tours returned from teh request
+  const noTours = true;
+
+  // Return response
+  res.status(200).render('pages/overview', {
+    title: 'Exciting Tours',
+    ...(tours.length === 0 || !tours ? { noTours } : { tours }),
+  });
+});
--- routers/publicRouter.js
+++ routers/publicRouter.js
@@ -1,11 +1,17 @@
 // MODULES IMPORT
 import express from 'express';
 
+import * as publicCtr from '../controllers/publicController.js';
+
 /// INIT ROUTER
 const router = express.Router();
 //@TODO: /overview & /:slug /login /logout /signup
 
 /// ROUTES
+/**
+ * Get Home page
+ */
+router.get('/', publicCtr.getOverview);
 
 /// EXPORT ROUTER
 export default router;
