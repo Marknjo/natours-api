5ac769304f17fa260be5879934bd75216b62d32d
Marknjo <marknjo7@gmail.com>
2022-03-17 10:48:26 +0300

    NR#Client: Add an error handler wrapper utility function

--- public/js/utils/codeWrappers.js
+++ public/js/utils/codeWrappers.js
@@ -8,13 +8,35 @@
  *
  * Keeps internal logic cleaner
  * @param {Promise} cb Generic callback function returned after import of a module
+ * @param {String} message Error message
  * @returns {void}
  */
-export const asyncImportWrapper = function (cb = Promise) {
+export const asyncImportWrapper = function (cb = Promise, message = '') {
   //
   return async function (event = Event) {
     try {
       await cb(event);
+    } catch (error) {
+      // TODO Add support for handling notification -> Error type here
+      console.log('Error submitting form');
+
+      // FIXME Remove this console log
+      console.log(error);
+    }
+  };
+};
+
+/**
+ * Abstracts error handling from the calling function
+ *
+ * @param {Function} cb Internal details of the calling function
+ * @param {String} message Error massage
+ * @returns {void}
+ */
+export const errorHandlerWrapper = function (cb = () => {}, message = '') {
+  return function (...args) {
+    try {
+      cb(...args);
     } catch (error) {
       // TODO Add support for handling notification -> Error type here
       console.log('Error submitting form');
