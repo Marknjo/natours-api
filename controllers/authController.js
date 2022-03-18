1b6b57d18f68a5c5f5180641eafd0e978f20a273
Marknjo <marknjo7@gmail.com>
2022-03-18 09:49:07 +0300

    NR#01-Client: Implement reusable cookie cookie options setter

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,6 +28,65 @@ const signJWTtoken = async (id, remember) => {
 };
 
 /**
+ * Helper function for setting cookies options
+ * @param {Request} req Express Request
+ * @param {{remember: boolean, allowRemember: boolean, customExpiresAt: number}} configOptions Cofiguration options -> Remember for login users or sining, and custom expires for custom cases where time is to be set
+ * @returns {T as object} -> Returns nothing. It is just a configuration utility
+ */
+const setCookieOption = (
+  req,
+  configOptions = { allowRemember: false, remember: false, customExpiresIn: 1 }
+) => {
+  // Initialize configs
+  const { customExpiresAt, remember, allowRemember } = {
+    ...(configOptions
+      ? {
+          allowRemember: false,
+          remember: false,
+          customExpiresIn: 1,
+          ...configOptions,
+        }
+      : { allowRemember: false, remember: false, customExpiresIn: 1 }),
+  };
+
+  console.log({
+    ...(configOptions
+      ? {
+          allowRemember: false,
+          remember: false,
+          customExpiresIn: 1,
+          ...configOptions,
+        }
+      : { allowRemember: false, remember: false, customExpiresIn: 1 }),
+  });
+
+  // Set timers
+  let expiresIn = new Date(Date.now() + 1000 * customExpiresAt);
+
+  /// Handle login and signup cases -> remember me
+  if (allowRemember) {
+    expiresIn = remember
+      ? new Date(Date.now() + 7 * 24 * 60 * 1000)
+      : new Date(Date.now() + 24 * 60 * 1000);
+  }
+
+  /// Define cookie options
+  const cookieOptions = {
+    expires: expiresIn,
+    httpOnly: true,
+    sameSite: true,
+  };
+
+  // Secure cookie check
+  // TODO: Requires more finetuning to handle live site
+  if (req.protocol === 'https' && env.NODE_ENV_NR === 'production')
+    cookieOptions.secure = true;
+
+  // Set cookie
+  return cookieOptions;
+};
+
+/**
  * Sign Token and Send response
  *
  * Does four functions
@@ -58,20 +117,12 @@ export const signTokenAndSendResponse = async (
     const jwtToken = await signJWTtoken(user.id, remember);
 
     // Add token to cookie response
-    // Cookie options
-    const cookieOptions = {
-      expires: remember
-        ? new Date(Date.now() + 7 * 24 * 60 * 1000)
-        : new Date(Date.now() + 24 * 60 * 1000),
-      httpOnly: true,
-      sameSite: true,
-    };
+    //setJwtCookie(req, res, jwtToken, { allowRemember: true, remember });
+    const cookieOptions = setCookieOption(req, {
+      allowRemember: true,
+      remember,
+    });
 
-    // Secure cookie check
-    if (req.protocol === 'https' && env.NODE_ENV_NR === 'production')
-      cookieOptions.secure = true;
-
-    // Set cookie
     res.cookie('jwt', jwtToken, cookieOptions);
 
     // Prep fields to respond
