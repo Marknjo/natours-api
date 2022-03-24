83b5fefcf4f13d315cd545256e34f0c1e8fb51e7
Marknjo <marknjo7@gmail.com>
2022-03-24 08:58:56 +0300

    NR#01-Client: Implement handling flash messages in the frontend

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@
 //import showLocationMap from './modules/locationsMap.js';
 
 import * as module from './importModules.js';
-import showAlert from './utils/showAlert.js';
+import handleFlashMessages from './modules/handleFlashMessages.js';
 
 /// GET DOM ELEMENTS
 /**
@@ -51,80 +51,7 @@ if (logoutEl) logoutEl.addEventListener('click', module.logoutHandler);
  */
 if (bodyEl) {
   const flashMessagesObj = bodyEl.dataset.flashMessages;
+  const userRole = bodyEl.dataset.userRole ? bodyEl.dataset.userRole : '';
 
-  /// Handle flash messages
-  if (flashMessagesObj) {
-    const flashMessages = JSON.parse(flashMessagesObj);
-
-    //console.log(flashMessages);
-
-    const handleFlashMessages = async flashMessages => {
-      // Flash message is set but has zero items in it, stop processing
-      if (flashMessages.length === 0) return;
-
-      console.table(flashMessages);
-
-      // console.table({
-      //   lenTest: flashMessages.length === 1,
-      //   locationTest: flashMessages[0].showOnPage === location.pathname,
-      //   flashMessageLocation: flashMessages[0].showOnPage,
-      //   currentLocation: location.pathname,
-      //   flashMessage: flashMessages[0],
-      // });
-
-      /// Flash messages bag has only one item
-      if (flashMessages.length === 1) {
-        // Process single item
-        const flashMessage = flashMessages[0];
-
-        // Handle admin messages differently
-        if (flashMessage.showOnPage.startsWith('/sys-admin')) {
-          /// Handle this message differently
-        }
-
-        // check which page it should be shown
-        if (flashMessage.showOnPage === location.pathname) {
-          // Only show those notifications here
-
-          // Filter time it was shown
-          showAlert({
-            ...flashMessage,
-            displayPosition: 'right',
-          });
-
-          console.log('running...');
-
-          const res = await fetch('/flash-shown', {
-            method: 'POST',
-            body: JSON.stringify(flashMessage),
-            headers: {
-              'Content-Type': 'application/json; charset=utf-8',
-            },
-          });
-
-          // TODO: Implement it only for admins otherwise fail silently
-          // Slow removal of the notification until the period set to remove is done.
-          if (!res.ok && res.status === 404) {
-            console.log('Could not update messages.');
-            showAlert({
-              message: 'Could not update messages.',
-              displayPosition: 'right',
-              messageType: 'info',
-            });
-          }
-
-          console.log(res);
-        }
-      }
-    };
-
-    handleFlashMessages(flashMessages);
-
-    /// TODO: FIlter server notifications
-    // showAlert({
-    //   ...message,
-    //   displayPosition: 'right',
-    //   messageType: 'success',
-    // });
-  }
+  handleFlashMessages(flashMessagesObj, userRole);
 }
---
+++ public/js/modules/handleFlashMessages.js
@@ -0,0 +1,75 @@
+import showAlert from '../utils/showAlert.js';
+
+/**
+ * Responsible showing notifications from the server and removing shown notification
+ * @param {{showOnPage: string, message: string,action: string,removeAfter: 'shown' | 'timeExpires', messageType: 'info' | 'warning' | 'success' | 'error', expiresIn: Date,createdAt: Date, }} flashMessage - Flash message object
+ * @param {string} userRole current logged in user role. For only showing admins if there is error
+ * @returns {never} Never returns anything
+ */
+const showFlashMessageAndRemoveShown = async (flashMessage, userRole) => {
+  // Only show notifications for the current page if removeAfte is timeExpires
+  if (flashMessage.showOnPage !== location.pathname) return;
+
+  // TODO: Handle admin messages differently
+  if (flashMessage.showOnPage.startsWith('/sys-admin')) {
+    /// Handle this message differently
+  }
+
+  // Show incoming notification before removing it
+  showAlert({
+    ...flashMessage,
+    displayPosition: 'right',
+  });
+
+  /// Only remove flash messages of they are identified as removeAfter shown
+  if (flashMessage.removeAfter === 'timeExpires') return;
+
+  const res = await fetch('/remove-shown-flash-message', {
+    method: 'POST',
+    body: JSON.stringify(flashMessage),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+
+  // Show notification if admin and response is not okay
+  if (userRole !== 'admin' && res.ok) return;
+
+  showAlert({
+    message: `Error showing the notification: ${flashMessage.message}`,
+    displayPosition: 'right',
+    messageType: 'info',
+  });
+};
+
+/**
+ * Handle showing and reseting of flash messages.
+ *
+ * Allows flash messages to remain for a while in the client if removeAfte timeExpires is set while those with removeAfter shown are removed immediately from the flash messages stored in the cookie
+ * @param {[{showOnPage: string, message: string,action: string,removeAfter: 'shown' | 'timeExpires', messageType: 'info' | 'warning' | 'success' | 'error', expiresIn: Date,createdAt: Date, }]} flashMessagesObj Collecton of flash messages stored in the express request
+ * @param {string} userRole current logged in user role. For only showing admins if there is error
+ * @returns {void | }
+ */
+const handleFlashMessages = async (flashMessagesObj, userRole) => {
+  // Do not proceed is we do not have a flash message objec with messages
+  if (!flashMessagesObj) return;
+
+  const flashMessages = JSON.parse(flashMessagesObj);
+
+  // Flash message is set but has zero items in it, stop processing
+  if (flashMessages.length === 0) return;
+
+  /// Cycle flash message
+  if (flashMessages.length > 1) {
+    flashMessages.forEach(flashMessage => {
+      showFlashMessageAndRemoveShown(flashMessage, userRole);
+    });
+
+    return;
+  }
+
+  /// Just a single flashMessage in the array
+  showFlashMessageAndRemoveShown(flashMessages[0], userRole);
+};
+
+export default handleFlashMessages;
