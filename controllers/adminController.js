56694ba462d5e309584bc50db31a1e938fb5655f
Marknjo <marknjo7@gmail.com>
2022-03-28 08:44:36 +0300

    NR#01-Client: Add support for dynamic active link for the dashboard navigation

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,8 +13,11 @@ export const getProfile = (req, res) => {
   // Get user by use
   const getUser = req.user ? `- ${req.user.name.split(' ')[0]}` : '';
 
+  console.log(req.originalUrl);
+
   res.status(200).render('pages/profile', {
     title: `Profile ${getUser}`,
+    pageUrl: req.originalUrl,
     assetsUrl: './../',
   });
 };
@@ -23,8 +26,11 @@ export const getProfile = (req, res) => {
  * Dash board Handler
  */
 export const getDashboard = catchAsync(async (req, res, next) => {
+  console.log(req.originalUrl);
+
   /// Successful response
   res.status(200).render('pages/dashboard', {
     title: 'Admin Dashboard',
+    pageUrl: req.originalUrl,
   });
 });
